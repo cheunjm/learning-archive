f0873180f0e8196a02c4211190cd1b95fc8597b5
Jaemin Cheun <cheunjm@gmail.com>
2019-03-28 12:24:28 +0900

    feat(globalStyle): set global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from "react-apollo";
 import App from "./Components/App";
+import GlobalStyle from "./global-styles";
 import React from "react";
 import ReactDOM from "react-dom";
 import client from "./apollo";
@@ -7,6 +8,7 @@ import client from "./apollo";
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
+    <GlobalStyle />
   </ApolloProvider>,
   document.getElementById("root") as HTMLElement
 );
