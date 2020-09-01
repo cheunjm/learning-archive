dea0448fb2392d32afe4eb09e9708b30306aa91c
Jaemin Cheun <jaemin@silviahealth.com>
2020-08-31 19:35:41 -0700

    typescript: finished sort

--- typescript/stats/src/index.ts
+++ typescript/stats/src/index.ts
@@ -1,12 +1,16 @@
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './inheritance/MatchResult';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+// Create an object that satisfies the Datareader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
